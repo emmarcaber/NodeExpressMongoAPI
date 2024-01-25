e36a61a5d542f3c2740f8a4f8b99a387ff7c6067
Emmar Caber <caberemmar@gmail.com>
2024-01-25 20:05:34 +0800

    Connect the app to MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
+// Connect to the database
+const mongoString = process.env.DATABASE_URL;
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", () => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
+// Start the server
 const app = express();
 
 app.use(express.json());
